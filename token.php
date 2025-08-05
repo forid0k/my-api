5152ad79889010515313c85e542ef231aeb0068b
forid0k <sshawon603@gmail.com>
2025-08-05 16:27:31 +0600

    Update and rename app.py to token.php

---
+++ token.php
@@ -0,0 +1,20 @@
+<?php
+header("Content-Type: application/json");
+
+$tokens = [
+    "123456789012",
+    "111222333444",
+    "555666777888"
+];
+
+if (isset($_GET['check'])) {
+    $checkToken = $_GET['check'];
+    if (in_array($checkToken, $tokens)) {
+        echo json_encode(["valid" => true]);
+        exit;
+    }
+    echo json_encode(["valid" => false]);
+    exit;
+}
+
+echo json_encode(["error" => "Please provide token with ?check=TOKEN"]);
